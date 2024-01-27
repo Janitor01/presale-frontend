26c6830253b61dd9fe65b3600b2e536c39fba2cd
Bug Bite <tempmailvoid@gmail.com>
2024-01-27 23:04:38 +0000

    update

--- frontend/src/config/environment.ts
+++ frontend/src/config/environment.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { alephzero } from '@scio-labs/use-inkathon'
 import { getSupportedChains } from './get-supported-chains'
 import { getURL } from './get-url'
 
@@ -7,10 +6,13 @@ import { getURL } from './get-url'
  * Environment Variables defined in `.env.local`.
  * See `env.local.example` for documentation.
  */
+
+
 export const env = {
+  
   url: getURL(),
   isProduction: process.env.NEXT_PUBLIC_PRODUCTION_MODE === 'true',
 
-  defaultChain: process.env.NEXT_PUBLIC_DEFAULT_CHAIN = 'alephzero',
+  defaultChain: process.env.NEXT_PUBLIC_DEFAULT_CHAIN!,
   supportedChains: getSupportedChains(),
 }
