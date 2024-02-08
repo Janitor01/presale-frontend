d6d8d2d8333cb3a4e2a7d6e48bf52c5e8af01174
Bug Bite <tempmailvoid@gmail.com>
2024-02-08 15:02:30 +0000

    updated name

--- frontend/src/app/components/home-page-title.tsx
+++ frontend/src/app/components/home-page-title.tsx
@@ -14,7 +14,7 @@ import { cn } from '@/utils/cn'
 
 
 export const HomePageTitle: FC = () => {
-  const title = '$IOU PRESALE'
+  const title = '$IOU PRESALE ROUND 2'
   const desc = 'Brought to you by Office Party'
   const seco = 'Mainnet Version'
   const [isExpanded, setIsExpanded] = useState(false);
