7bd2f61615b6eab17e43746a1b11df13eb2fb887
Bug Bite <tempmailvoid@gmail.com>
2024-02-08 15:12:52 +0000

    updated contract

--- contracts/deployments/bugbite/alephzero-testnet.ts
+++ contracts/deployments/bugbite/alephzero-testnet.ts
@@ -1,2 +1,2 @@
-export const address = '5GpBCWDN7toEfhLqhxkw2knkJPrVyPPSe9nBzZVEZnW1k3AS'
+export const address = '5Co5Ev8xZFhpXhoRVDeyHh6r1dRbRAdkVGeAwee3NBNZotY9'
 export const blockNumber = 41737105
