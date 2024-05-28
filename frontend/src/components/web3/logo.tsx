bfa80a45a9aa89129ee3c06d2776adb68a4a514f
Bug Bite <tempmailvoid@gmail.com>
2024-05-28 19:56:27 +0100

    updating

--- frontend/src/components/web3/logo.tsx
+++ frontend/src/components/web3/logo.tsx
@@ -8,13 +8,15 @@ import crown from 'public/images/crown.png'
 
 
 
+
+
 export const Logo: FC = () => {
   return (
     <div className='mt-10 max-w-4xl font-black'>
       <h1 className="mb-6 text-4xl font-black leading-[1.1] text-slate-200">
         TOKENOMICS <span className="text-indigo-500">$IOU</span>
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <div className="grid grid-cols-2 md:grid-cols-1 gap-4">
         <div>
           <h2 className='text-indigo-500 text-2xl'>Liquidity</h2>
           <h3>250,000,000</h3>
