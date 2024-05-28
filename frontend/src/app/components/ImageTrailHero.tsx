affc8dfbf1b48541f43151b20db7b34589cd495f
Bug Bite <tempmailvoid@gmail.com>
2024-05-28 14:37:53 +0100

    buy button

--- frontend/src/app/components/ImageTrailHero.tsx
+++ frontend/src/app/components/ImageTrailHero.tsx
@@ -89,7 +89,9 @@ const Copy = () => {
       <h1 className=" max-w-4xl text-6l font-black leading-[1.1] text-slate-900  md:text-8xl">
         BUY NOW
         </h1>
+        <div>
         <MagnetButton />
+        </div>
         </div>
       </div>
     <div className="absolute bottom-0 left-0 right-0  z-[1]">
